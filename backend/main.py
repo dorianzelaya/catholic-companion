55bb1446b2dab68f11f4576373fa44688c6ac0bb
dorianzelaya <dorianjzelaya@gmail.com>
2026-06-13 23:27:45 -0700

    connect PostgreSQL and create users table

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from database import engine, Base
+import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 

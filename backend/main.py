8045ae71a7ac18e4f30d43eb78eae57478bceeac
dorianzelaya <dorianjzelaya@gmail.com>
2026-08-03 00:05:14 -0700

    register user routes so /auth/me is available

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,8 @@ from routers import struggle_routes
 from routers.bible_routes import router as bible_router
 import models
 from routers.journal_routes import router as journal_router
+from routers.user_routes import router as user_router
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -29,6 +31,7 @@ app.include_router(readings_routes.router)
 app.include_router(struggle_routes.router)
 app.include_router(bible_router)
 app.include_router(journal_router)
+app.include_router(user_router)
 
 
 @app.get("/")
